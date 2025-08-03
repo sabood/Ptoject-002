f6d1ed613d435c36fac29ed2f0da76147dec9a39
sabood <saboodkuniyil@gmail.com>
2025-08-03 18:25:22 +0400

    count application files

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = []
+    if request.method == 'POST':
+        try:
+            start = int(request.form['start'])
+            end = int(request.form['end'])
+            if start > end:
+                result = ['Start number must be less than or equal to end number.']
+            else:
+                result = list(range(start, end + 1))
+        except ValueError:
+            result = ['Please enter valid numbers.']
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
