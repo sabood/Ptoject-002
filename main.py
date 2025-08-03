6e4c41bb6387007a94f34784a3c527f72e807606
sabood <saboodkuniyil@gmail.com>
2025-08-03 18:36:55 +0400

    Todo app

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+todo_list = []  # In-memory task list
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    result = []
-    if request.method == 'POST':
-        try:
-            start = int(request.form['start'])
-            end = int(request.form['end'])
-            if start > end:
-                result = ['Start number must be less than or equal to end number.']
-            else:
-                result = list(range(start, end + 1))
-        except ValueError:
-            result = ['Please enter valid numbers.']
-    return render_template('index.html', result=result)
+    return render_template('index.html', todos=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    if task:
+        todo_list.append({'task': task, 'done': False})
+    return redirect(url_for('index'))
+
+@app.route('/toggle/<int:index>')
+def toggle(index):
+    if 0 <= index < len(todo_list):
+        todo_list[index]['done'] = not todo_list[index]['done']
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    if 0 <= index < len(todo_list):
+        todo_list.pop(index)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000,debug=True)
